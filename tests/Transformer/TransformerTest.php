6599f1f32b78452ef4489b269245fb6073ddaa8a
Nyholm <tobias.nyholm@gmail.com>
2019-06-20 12:36:31 +0200

    Added tests

---
+++ tests/Transformer/TransformerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Happyr\MessageSerializer\Transformer;
+
+use Happyr\MessageSerializer\Transformer\Transformer;
+use Happyr\MessageSerializer\Transformer\TransformerInterface;
+use PHPUnit\Framework\TestCase;
+
+class TransformerTest extends TestCase
+{
+    public function testOutputArray()
+    {
+        $fooTransformer = $this->getMockBuilder(TransformerInterface::class)
+            ->setMethods(['getVersion', 'getIdentifier', 'getPayload', 'supports'])
+            ->getMock();
+
+        $version = 2;
+        $identifier = 'foobar';
+        $payload = ['foo' => 'bar'];
+
+        $fooTransformer->method('getVersion')->willReturn($version);
+        $fooTransformer->method('getIdentifier')->willReturn($identifier);
+        $fooTransformer->method('supports')->willReturn(true);
+        $fooTransformer->method('getPayload')->willReturn($payload);
+
+        $transformer = new Transformer([$fooTransformer]);
+        $output = $transformer->toArray(new \stdClass());
+
+        $this->assertArrayHasKey('version', $output);
+        $this->assertArrayHasKey('identifier', $output);
+        $this->assertArrayHasKey('timestamp', $output);
+        $this->assertArrayHasKey('payload', $output);
+
+        $this->assertEquals($output['version'], $version);
+        $this->assertEquals($output['identifier'], $identifier);
+        $this->assertEquals($output['payload'], $payload);
+        $this->assertEquals($output['timestamp'], time(), '', 3);
+    }
+}
