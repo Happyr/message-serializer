add441e6ee987a57475761afb22a89189ccae77b
Radoje Albijanic <radoje.albijanic@gmail.com>
2020-03-04 09:51:00 +0100

    Tests for serializing with retry count (#34)

--- tests/SerializerTest.php
+++ tests/SerializerTest.php
@@ -9,6 +9,7 @@ use Happyr\MessageSerializer\Serializer;
 use Happyr\MessageSerializer\Transformer\MessageToArrayInterface;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Stamp\RedeliveryStamp;
 
 /**
  * @internal
@@ -39,6 +40,35 @@ final class SerializerTest extends TestCase
         self::assertInstanceOf(\stdClass::class, $output->getMessage());
     }
 
+    public function testDecodeWithRetryCount(): void
+    {
+        $transformer = $this->getMockBuilder(MessageToArrayInterface::class)->getMock();
+        $hydrator = $this->getMockBuilder(ArrayToMessageInterface::class)
+            ->getMock();
+
+        $payload = [
+            'a' => 'b',
+        ];
+        $data = [
+            'body' => \json_encode(
+                array_merge($payload, ['_meta' => ['retry-count' => 2]]),
+                JSON_THROW_ON_ERROR, 512),
+        ];
+
+        $hydrator->expects(self::once())
+            ->method('toMessage')
+            ->with($payload)
+            ->willReturn(new \stdClass());
+
+        $serializer = new Serializer($transformer, $hydrator);
+        $output = $serializer->decode($data);
+
+        self::assertInstanceOf(\stdClass::class, $output->getMessage());
+        /** @var RedeliveryStamp $redeliveryStamp */
+        $redeliveryStamp = $output->last(RedeliveryStamp::class);
+        self::assertEquals(2, $redeliveryStamp->getRetryCount());
+    }
+
     public function testEncode()
     {
         $transformer = $this->getMockBuilder(MessageToArrayInterface::class)
@@ -62,5 +92,37 @@ final class SerializerTest extends TestCase
 
         self::assertArrayHasKey('body', $output);
         self::assertEquals(\json_encode(['foo' => 'bar', '_meta' => []]), $output['body']);
+    }
+
+    public function testEncodeWithRedeliveryStamp()
+    {
+        $transformer = $this->getMockBuilder(MessageToArrayInterface::class)
+            ->getMock();
+        $hydrator = $this->getMockBuilder(ArrayToMessageInterface::class)->getMock();
+
+        $envelope = new Envelope(new \stdClass('foo'), [new RedeliveryStamp(2)]);
+
+        $transformer->expects(self::once())
+            ->method('toArray')
+            ->with($envelope)
+            ->willReturn(['foo' => 'bar']);
+
+        $serializer = new Serializer($transformer, $hydrator);
+        $output = $serializer->encode($envelope);
+
+        self::assertArrayHasKey('headers', $output);
+        self::assertArrayHasKey('Content-Type', $output['headers']);
+        self::assertEquals('application/json', $output['headers']['Content-Type']);
+
+        self::assertArrayHasKey('body', $output);
+        self::assertEquals(\json_encode(
+            [
+                'foo' => 'bar',
+                '_meta' => [
+                    'retry-count' => 2,
+                ],
+            ], JSON_THROW_ON_ERROR, 512),
+            $output['body']
+        );
     }
 }
