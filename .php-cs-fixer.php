d1fa36763e6da6c32f767399d305bbefec0b27b4
Simon Mönch <simonmoench@googlemail.com>
2022-01-07 10:28:17 +0100

    another fix for php-cs-fixer v3

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -5,7 +5,8 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__.'/tests')
 ;
 
-return PhpCsFixer\Config::create()
+$config = new PhpCsFixer\Config();
+return $config
     ->setRules([
         '@Symfony' => true,
         'array_syntax' => ['syntax' => 'short'],
