5d845fa80bb6a6b414853d831361d05a05d69abf
Tobias Nyholm <tobias.nyholm@gmail.com>
2022-07-21 10:58:57 +0200

    Add logger to Serializer (#50)

--- src/Serializer.php
+++ src/Serializer.php
@@ -7,6 +7,8 @@ namespace Happyr\MessageSerializer;
 use Happyr\MessageSerializer\Hydrator\ArrayToMessageInterface;
 use Happyr\MessageSerializer\Hydrator\Exception\HydratorException;
 use Happyr\MessageSerializer\Transformer\MessageToArrayInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Exception\MessageDecodingFailedException;
 use Symfony\Component\Messenger\Stamp\NonSendableStampInterface;
@@ -17,11 +19,13 @@ final class Serializer implements SerializerInterface
 {
     private $transformer;
     private $hydrator;
+    private $logger;
 
-    public function __construct(MessageToArrayInterface $transformer, ArrayToMessageInterface $hydrator)
+    public function __construct(MessageToArrayInterface $transformer, ArrayToMessageInterface $hydrator, LoggerInterface $logger = null)
     {
         $this->transformer = $transformer;
         $this->hydrator = $hydrator;
+        $this->logger = $logger ?? new NullLogger();
     }
 
     /**
@@ -30,12 +34,14 @@ final class Serializer implements SerializerInterface
     public function decode(array $encodedEnvelope): Envelope
     {
         if (empty($encodedEnvelope['body'])) {
+            $this->logger->error('Failed to decode message with no body.');
             throw new MessageDecodingFailedException('Encoded envelope should have at least a "body".');
         }
 
         try {
             $array = \json_decode($encodedEnvelope['body'], true, 512, \JSON_THROW_ON_ERROR);
         } catch (\JsonException $e) {
+            $this->logger->error('Failed to run json_decode on message.', ['exception' => $e]);
             throw new MessageDecodingFailedException(\sprintf('Error when trying to json_decode message: "%s"', $encodedEnvelope['body']), 0, $e);
         }
 
@@ -46,6 +52,7 @@ final class Serializer implements SerializerInterface
             $message = $this->hydrator->toMessage($array);
             $envelope = $message instanceof Envelope ? $message : new Envelope($message);
         } catch (HydratorException $e) {
+            $this->logger->error('Failed to run hydrate message to object.', ['exception' => $e, 'identifier' => $array['identifier'] ?? '(no identifier)', 'version' => $array['version'] ?? '(no version)']);
             throw new MessageDecodingFailedException('Failed to decode message', 0, $e);
         }
 
